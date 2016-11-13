6aa02e75e2a1dbba0e81b7301bf3644b3d9c7a19
Honain Khan <honaink@gmail.com>
2016-11-13 16:57:50 -0600

    added getting time series data

--- tests/tests.py
+++ tests/tests.py
@@ -11,10 +11,13 @@ app.add_metric(metric_2)
 
 env.add_application(app)
 
-available_counter_metrics = get_counter_metadata()
-for env in available_counter_metrics:
-    print(env.environment_name)
-    for app in env.apps:
-        print('-' + app.application_name)
-        for metric in app.metrics:
-            print('--' + metric.metric_name)
+# available_counter_metrics = get_counter_metadata()
+# for env in available_counter_metrics:
+#     print(env.environment_name)
+#     for app in env.apps:
+#         print('-' + app.application_name)
+#         for metric in app.metrics:
+#             print('--' + metric.metric_name)
+
+result = get_timer_metric_timeseries(['mean', 'p99'], 'dev-dfsp1', 'interop-spsp-clientproxy', 'l1p.spsp.payments.api.PaymentsPutProxyTime', '2011-02-03 04:05')
+[print(row) for row in result]
