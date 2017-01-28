96c21e54b869a3be24ff5c1d213fd0d7d828efb7
Alan Vezina <alan.vezina@modusbox.com>
2017-01-27 17:40:48 -0800

    Only select specific columns in get_metric_data
    - TODO: update metrics_controller to use the new API

--- metrics_server/metrics_service.py
+++ metrics_server/metrics_service.py
@@ -7,12 +7,24 @@ from cassandra.query import dict_factory
 from metrics_server.base_service import BaseService
 from metrics_server.errors import NotFoundError
 
+
+"""
+Because a query can return a very large set of data, we need to find out how to aggregate said data, ideally without
+losing the ability to find anomalies. The pandas library looks to provide a pretty simple to use API for aggregating
+time series data, and it looks like we can easily integrate our Cassandra queries into Pandas data frames:
+    http://pandas-docs.github.io/pandas-docs-travis/timeseries.html
+    http://stackoverflow.com/questions/41247345/python-read-cassandra-data-into-pandas
+    http://blog.yhat.com/posts/aggregating-and-plotting-time-series-in-python.html
+    http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.resample.html
+    http://pandas.pydata.org/pandas-docs/stable/timeseries.html#frequency-conversion
+"""
+
 KEYSPACE = 'metric_data'
 TABLE_NAMES = ['raw_counter_with_interval', 'raw_timer_with_interval']
 TIMESTAMP_COLUMNS = ['metric_timestamp', 'previous_metric_timestamp']
-COUNTER_COLUMNS = ['count', 'previous_count']
-TIMER_COLUMNS = ['count', 'previous_count', 'p75', 'p95', 'p98', 'p99', 'p999', 'one_min_rate', 'fifteen_min_rate',
-                 'five_min_rate', 'max', 'mean', 'mean_rate', 'median', 'min', 'std_dev']
+COUNTER_COLUMNS = {'count', 'previous_count'}
+TIMER_COLUMNS = {'count', 'previous_count', 'p75', 'p95', 'p98', 'p99', 'p999', 'one_min_rate', 'fifteen_min_rate',
+                 'five_min_rate', 'max', 'mean', 'mean_rate', 'median', 'min', 'std_dev'}
 COLUMN_MAP = {
     'raw_counter_with_interval': COUNTER_COLUMNS,
     'raw_timer_with_interval': TIMER_COLUMNS
@@ -115,7 +127,8 @@ class MetricsService(BaseService):
 
         return available_metrics
 
-    def get_metrics_data(self, environment, application, table, metric, start_timestamp=None, end_timestamp=None):
+    def get_metrics_data(self, environment, application, table, metric, columns, start_timestamp=None,
+                         end_timestamp=None):
         """
         Retrieve data from DB for a given environment, application, and metric.
 
@@ -125,12 +138,13 @@ class MetricsService(BaseService):
         :param application: The application you want to retrieve data from.
         :param table: The table you want to retrieve data from.
         :param metric: The metric you want to retrieve data from.
+        :param columns: The columns to select.
         :param start_timestamp: The timestamp you want to start retrieving data for. Defaults to 24 hours ago.
         :param end_timestamp: The timestamp you want to receive data until. Defaults to now.
         :return: list of rows.
         """
         if end_timestamp is None:
-            end_timestamp = datetime.now(tz=pytz.timezone('GMT'))
+            end_timestamp = datetime.now(tz=pytz.utc)
 
         if start_timestamp is None:
             start_timestamp = end_timestamp - timedelta(hours=24)
@@ -141,15 +155,25 @@ class MetricsService(BaseService):
         if table_columns is None:
             raise NotFoundError(f'table "{table}" does not exist.')
 
-        all_columns = table_columns + TIMESTAMP_COLUMNS
-        column_str = ', '.join(all_columns)
-        # Hard limit of 100 right now because without timestamp filters it's super easy to get ~100k rows.
-        # TODO: add a start_timestamp and end_timestamp
-        query = f"""SELECT {column_str} FROM {table} WHERE environment = %s AND application = %s AND metric_name= %s
-        AND metric_timestamp >= %s AND metric_timestamp <= %s;"""
+        for column in columns:
+            if column not in table_columns:
+                raise ValueError(f'Column "{column}" is not available for table "{table}"')
+
+        columns.append('metric_timestamp')
+        column_str = ', '.join(columns)
+        query = f"""
+        SELECT {column_str} FROM {table} WHERE environment = %s AND application = %s AND metric_name= %s
+        AND metric_timestamp >= %s AND metric_timestamp <= %s;
+        """
         rows = self.session.execute(query, [environment, application, metric, start_timestamp, end_timestamp])
 
         for row in rows:
+            # Convert the timestamps to ISO 8601 because it's less verbose than the default Flask representation
+            row['metric_timestamp'] = pytz.utc.localize(row['metric_timestamp']).isoformat()
+
+            if 'previous_metric_timestamp' in row:
+                row['previous_metric_timestamp'] = pytz.utc.localize(row['previous_metric_timestamp']).isoformat()
+
             data.append(row)
 
         return data
