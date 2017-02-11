a7026c0f3f4606113cd83753734005fc391771be
Alan Vezina <alan.vezina@modusbox.com>
2017-02-10 16:11:42 -0800

    TimeSeriesChart - Stub out functionality for working with Rickshaw
    - Need to tell React to not render the component so we can have Rickshaw own the DOM inside of TimeSeriesChart

--- frontend/src/components/TimeSeriesChart.js
+++ frontend/src/components/TimeSeriesChart.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-
+// import Rickshaw from 'rickshaw';
 
 export class TimeSeriesChart extends Component {
+  componentDidMount() {
+    this.chartEl = this.refs.chart;
+    this.renderChart(this.props);
+  }
+
+  static shouldComponentUpdate() {
+    return false;
+  }
+
+  componentWillReceiveProps(props) {
+    this.renderChart(props);
+  }
+
+  renderChart(props) {
+    const chart = props.chart;
+    const text = `Need to plot ${chart.metrics.map(metric => metric.metric_name).join(', ')}`;
+    this.chartEl.textContent = text;
+  }
+
   render() {
     return (
-      <div className="time-series">
-        Need to plot {this.props.metrics.map(metric => metric.metric_name).join(', ')}
+      <div className="time-series" ref="chart">
       </div>
     );
   }
