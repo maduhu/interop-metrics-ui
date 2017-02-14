0e18c10b3c9de6b7f98d0fe9fd12f588656fdfe3
Alan Vezina <alan.vezina@modusbox.com>
2017-02-13 18:01:35 -0800

    TimeSeriesChart - Actually render chart with Rickshaw

--- frontend/src/components/TimeSeriesChart.js
+++ frontend/src/components/TimeSeriesChart.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-// import Rickshaw from 'rickshaw';
+import moment from 'moment';
+import Rickshaw from 'rickshaw';
+import 'rickshaw/rickshaw.css';
+
+// This color palette was stolen from d3.js 4.0 d3.schemeCategory10
+const COLORS = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22",
+  "#17becf"];
 
 export class TimeSeriesChart extends Component {
+  constructor(props) {
+    super(props);
+    this.updateSeries = this.updateSeries.bind(this);
+    this.renderChart = this.renderChart.bind(this);
+  }
+
   componentDidMount() {
     this.chartEl = this.refs.chart;
+    this.series = [];
+    this.graph = new Rickshaw.Graph({
+      element: this.chartEl,
+      stack: false,
+      renderer: 'line',
+      series: this.series,
+    });
+    this.xAxis = new Rickshaw.Graph.Axis.Time({graph: this.graph});
     this.renderChart(this.props);
   }
 
@@ -15,17 +35,42 @@ export class TimeSeriesChart extends Component {
     this.renderChart(props);
   }
 
-  renderChart(props) {
+  updateSeries(props) {
+    /**
+     * Update series requires us to mutate the existing series object, because as far as I can tell there is no way to
+     * give Rickshaw new data, instead you must update the data object that you have.
+     * @type {Object}
+     */
     const chart = props.chart;
-    const text = `Need to plot ${chart.metrics.map(metric => metric.metric_name).join(', ')}`;
-    this.chartEl.textContent = text;
+    const series = this.series;
+
+    series.splice(0, series.length);
+
+    return Object.keys(chart.data).forEach((measure, idx) => {
+      const measurePieces = measure.split('.');
+      const measureName = measurePieces[measurePieces.length - 1];
+
+      series.push({
+        name: measure,
+        color: COLORS[idx % COLORS.length],
+        data: chart.data[measure].map((row) => {
+          return {
+            x: moment(row.metric_timestamp).unix(),
+            y: row[measureName]
+          }
+        }),
+      });
+    });
+  }
+
+  renderChart(props) {
+    this.updateSeries(props);
+    this.graph.render();
+    this.xAxis.render();
   }
 
   render() {
-    return (
-      <div className="time-series" ref="chart">
-      </div>
-    );
+    return <div className="time-series" ref="chart"></div>;
   }
 }
 
