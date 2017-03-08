b31e0a056d6f13ac7839d2625ec9f43bc1fbf978
Alan Vezina <alan.vezina@modusbox.com>
2017-03-07 18:20:16 -0800

    DataFrameEncoder - use 'split' orientation on to_dict

--- metrics_server/data_frame_encoder.py
+++ metrics_server/data_frame_encoder.py
@@ -7,19 +7,11 @@ class DataFrameEncoder(JSONEncoder):
     Encodes DataFrames to JSON.
 
     Normally you'd just use DataFrame.to_json, however, if you want to nest your DataFrame in a larger JSON object you
-    cannot do that, and have to implement your own JSON encoder. Also by implementing our own Encoder we can use the
-    to_dict method with 'list' orientation, which gives us an object that looks like this:
-        {
-          "column_1": [value_1, value_2, ..., value_n],
-          ...
-          "column_n": [value_1, value_2, ..., value_n]
-        }
+    cannot do that, and have to implement your own JSON encoder.
     """
     def default(self, obj):
         if isinstance(obj, DataFrame):
-            # TODO: change orient to 'list' and update front end to handle new format. If we use list orientation the
-            # JSON body will be significantly smaller in size.
-            return obj.where(notnull(obj), None).to_dict(orient='records')
+            return obj.where(notnull(obj), None).to_dict(orient='split')['data']
 
         if isinstance(obj, Timestamp):
             return to_datetime(obj).isoformat()
