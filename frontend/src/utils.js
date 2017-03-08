69d4b16c861aa1af50e8365ab5b55f088d63731f
Alan Vezina <alan.vezina@modusbox.com>
2017-03-07 18:21:56 -0800

    utils.js - Add has method (alias of hasOwnProperty)
    - Now we conform with AirBnB style guide.

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,3 +1,5 @@
 export function generateMetricsKey(metric) {
   return `${metric.environment}.${metric.application}.${metric.metric_name}.${metric.measure}`;
 }
+
+export const has = Object.prototype.hasOwnProperty;
