b494ea741eafc3a44d1869379f5defcd26fa0b9d
Alan Vezina <alan.vezina@modusbox.com>
2017-03-12 00:09:26 -0800

    utils.js - Add generateMetricsUrl

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,5 +1,9 @@
-export function generateMetricsKey(metric) {
-  return `${metric.environment}.${metric.application}.${metric.metric_name}.${metric.measure}`;
+export function generateMetricsKey(m) {
+  return `${m.environment}.${m.application}.${m.metric_name}.${m.measure}`;
+}
+
+export function generateMetricsUrl(m) {
+  return `api/v1/environments/${m.environment}/applications/${m.application}/metrics/${m.table}/${m.metric_name}`;
 }
 
 export const has = Object.prototype.hasOwnProperty;
