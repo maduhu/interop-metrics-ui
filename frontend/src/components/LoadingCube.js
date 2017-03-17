d301094ac4c529b359846adee6ffe013fc663d24
Alan Vezina <alan.vezina@modusbox.com>
2017-03-17 14:00:23 -0700

    LoadingCube - convert to PureComponent

--- frontend/src/components/LoadingCube.js
+++ frontend/src/components/LoadingCube.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import './LoadingCube.css';
 
-export class LoadingCube extends Component {
+export class LoadingCube extends PureComponent {
   render() {
     return (
       <div className="loading-cube">
         <div className="sk-cube-grid">
-          <div className="sk-cube sk-cube1"></div>
-          <div className="sk-cube sk-cube2"></div>
-          <div className="sk-cube sk-cube3"></div>
-          <div className="sk-cube sk-cube4"></div>
-          <div className="sk-cube sk-cube5"></div>
-          <div className="sk-cube sk-cube6"></div>
-          <div className="sk-cube sk-cube7"></div>
-          <div className="sk-cube sk-cube8"></div>
-          <div className="sk-cube sk-cube9"></div>
+          <div className="sk-cube sk-cube1" />
+          <div className="sk-cube sk-cube2" />
+          <div className="sk-cube sk-cube3" />
+          <div className="sk-cube sk-cube4" />
+          <div className="sk-cube sk-cube5" />
+          <div className="sk-cube sk-cube6" />
+          <div className="sk-cube sk-cube7" />
+          <div className="sk-cube sk-cube8" />
+          <div className="sk-cube sk-cube9" />
         </div>
         <div className="loading-cube__text">
           {this.props.children}
