bf77e086a7a22b91b748680f2dae4d63ba026864
Alan Vezina <alan.vezina@modusbox.com>
2017-03-21 12:23:26 -0700

    build_images.sh - export docker images

--- build_images.sh
+++ build_images.sh
@@ -4,6 +4,11 @@ echo Building frontend assets...
 cd frontend
 npm run build
 cd ..
+
 echo Building docker images...
 docker-compose build
+
+echo Exporting images to metrics_server_bundle.tar
+docker save -o metrics_server_bundle.tar metricdata_web_1 metricdata_web_2 metricdata_web_3 metricdata_web_4 metricdata_load_balancer
+
 echo Build complete
