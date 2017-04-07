a9c320a29f11b5b6a0943dab9c68e31099779436
Alan Vezina <alan.vezina@modusbox.com>
2017-04-06 17:16:10 -0700

    Don't try to save non-existent containers

--- build_images.sh
+++ build_images.sh
@@ -9,6 +9,6 @@ echo Building docker images...
 docker-compose build
 
 echo Exporting images to metrics_server_bundle.tar.gz
-docker save metricdata_web_1 metricdata_web_2 metricdata_web_3 metricdata_web_4 metricdata_load_balancer | gzip -c > metrics_server_bundle.tar.gz
+docker save metricdata_web_1 metricdata_web_2 metricdata_load_balancer | gzip -c > metrics_server_bundle.tar.gz
 
 echo Build complete
