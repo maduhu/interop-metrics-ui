f7c018bf9028a490a0c02576fc0dc9b895b9a476
Alan Vezina <alan.vezina@modusbox.com>
2017-05-11 13:41:08 -0700

    Improve NewDashboardDialog
    - Focus name input on dialog open
    - Add dashboard when user hits enter

--- frontend/src/components/NewDashboardDialog.js
+++ frontend/src/components/NewDashboardDialog.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import Dialog from './Dialog';
 import './NewDashboardDialog.css';
 
+const ENTER = 13;
+
 export default class NewDashboardDialog extends Component {
   constructor(props) {
     super(props);
+    this.handleEnter = this.handleEnter.bind(this);
 
     this.state = {
       name: '',
     };
+  }
+
+  componentDidMount() {
+    this.nameInput.focus();
+  }
+
+  handleEnter(e) {
+    if (e.keyCode === ENTER) {
+      this.props.save(this.state.name);
+    }
   }
 
   render() {
@@ -28,7 +41,9 @@ export default class NewDashboardDialog extends Component {
             className="new-dashboard-form__input"
             type="text"
             value={this.state.name}
+            ref={(input) => { this.nameInput = input; }}
             onChange={e => this.setState({ name: e.target.value })}
+            onKeyDown={this.handleEnter}
           />
         </div>
       </Dialog>
