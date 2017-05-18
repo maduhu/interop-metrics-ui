8ce88d48d9d246b8801ada09f896441491f7f697
Alan Vezina <alan.vezina@modusbox.com>
2017-05-17 17:12:33 -0700

    Add MetricsServerException type
    - Strips ' characters from the beginning and end of an exception when the str method is used.

--- metrics_server/errors.py
+++ metrics_server/errors.py
@@ -1,6 +1,15 @@
-class NotFoundError(KeyError):
+class MetricsServerException(Exception):
+    """
+    This exception class overrides the __str__ method to strip extraneous ' characters that certain exception types like
+    to add for reasons completely unknown to me.
+    """
+    def __str__(self):
+        return super().__str__().strip("'")
+
+
+class NotFoundError(MetricsServerException, KeyError):
     pass
 
 
-class ConfigurationError(KeyError):
+class ConfigurationError(MetricsServerException, KeyError):
     pass
